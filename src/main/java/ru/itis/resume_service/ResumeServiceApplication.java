c5d2fc8db049225046f63c92628157789971adc1
Renat <renat.blinov@yandex.ru>
2020-10-06 13:03:34 +0300

    Added main class

---
+++ src/main/java/ru/itis/resume_service/ResumeServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.itis.resume_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResumeServiceApplication.class, args);
+    }
+
+}
