be4f681ff89769111b34f8693381635ab64df579
Renat <renat.blinov@yandex.ru>
2020-10-07 18:24:33 +0300

    Added swagger2 annotation to main class

--- src/main/java/ru/itis/resume_service/ResumeServiceApplication.java
+++ src/main/java/ru/itis/resume_service/ResumeServiceApplication.java
@@ -2,7 +2,9 @@ package ru.itis.resume_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class ResumeServiceApplication {
 
