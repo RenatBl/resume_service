7dafb236da5c2518498acfa8f06c9934d3609b4b
Renat <renat.blinov@yandex.ru>
2020-10-07 18:30:00 +0300

    Some changes

--- src/main/java/ru/itis/resume_service/forms/ResumeUpdateForm.java
+++ src/main/java/ru/itis/resume_service/forms/ResumeUpdateForm.java
@@ -1,5 +1,7 @@
 package ru.itis.resume_service.forms;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,7 +17,9 @@ import java.util.List;
 @NoArgsConstructor
 public class ResumeUpdateForm {
 
+    @JsonSerialize(using= ToStringSerializer.class)
     private ObjectId id;
+
     private String jobTitle;
     private List<WorkExperience> workExperiences;
     private String employmentType;
