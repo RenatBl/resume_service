8ff089a275abfa987bf603d22b3d6021cd1e8829
Renat <renat.blinov@yandex.ru>
2020-10-07 14:35:13 +0300

    User model updated

--- src/main/java/ru/itis/resume_service/models/User.java
+++ src/main/java/ru/itis/resume_service/models/User.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -17,12 +18,22 @@ import ru.itis.resume_service.models.enums.Role;
 public class User {
 
     @Id
-    private Long id;
+    private ObjectId id;
 
-    @Indexed
+    @Indexed(unique = true)
     private String email;
 
     private String password;
 
     private Role role;
+
+    private String name;
+
+    private String surname;
+
+    private Integer age;
+
+    private String phoneNumber;
+
+    private String settlement;
 }
